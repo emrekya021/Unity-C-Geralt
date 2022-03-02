e80c85c52a80580102fc34b58a9c33cb3856704a
Emre Kaya <59964776+emrekya021@users.noreply.github.com>
2022-03-02 17:56:27 +0300

    Geralt

---
+++ console-programlama/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace console_programlama
+{
+      
+      class Program
+      {
+
+          public static void Main(string[] args){
+
+            Console.WriteLine("Hoşgeldin Witcher");
+            Console.WriteLine("Adını söyle geçmene izin veriyim ?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Son olarak nereden geldin "+ name + " ?");
+            string country = Console.ReadLine();
+            Console.WriteLine("Geçmene izin veriyorum " + country + "'lı " + name);
+
+          }
+      }
+}
